6b2d7e231a7cee51ce1a301443d5f31e3fe74533
Samet Arda Erdogan <sametarda.dev@gmail.com>
2022-05-24 13:30:51 +0300

    Create Homework3.sql

---
+++ SQL/Homework3.sql
@@ -0,0 +1,22 @@
+
+-- Q1 --
+
+SELECT country FROM country
+WHERE country LIKE 'A%a';
+
+-- Q2 --
+
+SELECT country FROM country
+WHERE country LIKE '_____%n';
+
+-- Q3 --
+
+SELECT * FROM film
+WHERE title ILIKE '%T%T%T%T' OR title ILIKE 'T%T%T%T%';
+
+-- Q4 --
+
+SELECT * FROM film
+WHERE title LIKE 'C%'
+AND length > 90
+AND rental_rate = 2.99;
