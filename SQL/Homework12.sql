e197e9dbf4fccc6d95ae35fcd87ba4ea1732bb96
Samet Arda Erdogan <sametarda.dev@gmail.com>
2022-05-27 14:53:33 +0300

    Create Homework12.sql

---
+++ SQL/Homework12.sql
@@ -0,0 +1,22 @@
+-- Q1 --
+
+SELECT title,length FROM film
+WHERE length > (SELECT AVG(length) FROM film);
+
+-- Q2 --
+
+SELECT COUNT(*) FROM film
+WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+-- Q3 --
+SELECT title FROM film
+WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) 
+AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+-- Q4 --
+
+SELECT 
+COUNT(payment_id),
+(SELECT first_name FROM customer WHERE payment.customer_id=customer.customer_id) FROM payment
+GROUP BY customer_id 
+ORDER BY COUNT(payment_id) DESC;
